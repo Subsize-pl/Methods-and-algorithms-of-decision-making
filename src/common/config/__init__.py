854727ab74cc0b16a8a1b606f3006255b9db6f0c
eduard plyushchevich <eduardatapovic@gmail.com>
2026-03-15 21:18:42 +0300

    Refactor code

---
+++ src/common/config/__init__.py
@@ -0,0 +1,7 @@
+from .log import LoggingSettings as LogSettings
+from .data_generator import DataGeneratorSettings as DgSettings
+
+__all__ = (
+    "LogSettings",
+    "DgSettings",
+)
---
+++ src/common/config/data_generator.py
@@ -0,0 +1,9 @@
+class DataGeneratorSettings:
+    MIN_SCALE = 1
+    MAX_SCALE = 12
+
+    MIN_X = -50
+    MAX_X = 50
+
+    MAX_MODES = 20
+    MIN_MODES = 3
